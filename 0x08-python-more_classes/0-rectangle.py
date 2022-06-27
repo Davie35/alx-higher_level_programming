d4e98a3ea934c2dce2892c036dedd8bc367aa214
Davie35 <dnjoroge35@gmail.com>
2022-06-27 22:52:26 +0300

    Write an empty class Rectangle that defines a rectangle:

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""
-This is a Rectangle class.
-"""
+class Rectangle():
+    def __init__(self, l, w):
+        self.length = l
+        self.width  = w
 
+    def rectangle_area(self):
+        return self.length*self.width
 
-class Rectangle:
-    """
-    Create an empty Rectangle Object.
-    """
-    pass
+newRectangle = Rectangle(12, 10)
+print(newRectangle.rectangle_area())
