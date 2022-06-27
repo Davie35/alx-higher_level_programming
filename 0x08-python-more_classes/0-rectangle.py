dc38f5588028f85b1c8a13489e3e52fe3c29bc64
Davie35 <dnjoroge35@gmail.com>
2022-06-27 23:31:54 +0300

    Write an empty class Rectangle that defines a rectangle:

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-"""An empty rectangle class"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """Property retriever, for retreiving"""
-    pass)
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
+
