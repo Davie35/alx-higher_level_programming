b84a146c24f55060cf463c675432b311eee275bb
Davie35 <dnjoroge35@gmail.com>
2022-07-05 22:26:50 +0300

    function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""Load, add, save
-A script that adds all arguments to a Python list,
-and then save them to a file
-"""
-import sys
+'''function that returns the dictionary description with simple data structure
+'''
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+def class_to_json(obj):
+    '''module class_to_json
+       returns builds a dictionary
+    '''
+    return obj.__dict__
